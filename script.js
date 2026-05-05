0636f6963bc7e269e5603c2fcdafdaac668d3150
Arpit kumar <arpitkumar8790@gmail.com>
2026-05-05 23:06:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,32 @@ window.addEventListener("scroll", function () {
         if (position < screenPosition) {
             el.classList.add("show");
         } else {
-            el.classList.remove("show"); // ADD THIS LINE
+            el.classList.remove("show");
         }
     });
+});
+
+// ✅ EmailJS Integration (ALAG SE, niche)
+
+// Initialize EmailJS
+(function(){
+    emailjs.init("pX5Gihd0lDvItPtNj"); // yaha apni key daalna
+})();
+
+// Form submit
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.getElementById("contact-form");
+
+    if(form){
+        form.addEventListener("submit", function(e) {
+            e.preventDefault();
+
+            emailjs.sendForm("service_e0n1r5l", "template_2f893hk", this)
+            .then(function() {
+                alert("Message Sent Successfully ✅");
+            }, function(error) {
+                alert("Failed ❌");
+            });
+        });
+    }
 });
